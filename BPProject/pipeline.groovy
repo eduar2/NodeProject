d12464eb62684e8a2f210f8b408437d73ce33f79
eduar2 <eduar2@localhost.localdomain>
2019-06-16 18:39:06 -0500

    Correct jenkins

--- BPProject/pipeline.groovy
+++ BPProject/pipeline.groovy
@@ -4,7 +4,7 @@ node {
     }
 
     stage('Build') {
-        sh """
+        sh '''
             if [  '$(docker ps -q -f name=devops-container1' ]; then
                             docker rm -f devops-container1
             fi
@@ -16,7 +16,7 @@ node {
             fi
             
             docker-compose up -d --build
-        """
+        '''
     }
   
     stage('End ') {
