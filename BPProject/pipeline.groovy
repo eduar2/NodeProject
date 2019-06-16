02a01d7637e3d1f18d63ab90f3c227ee36ceb619
eduar2 <eduar2@localhost.localdomain>
2019-06-16 18:24:44 -0500

    Correct jenkins

--- BPProject/pipeline.groovy
+++ BPProject/pipeline.groovy
@@ -5,7 +5,6 @@ node {
 
     stage('Build') {
         sh '''
-            echo Delete old containers
             if [  "$(docker ps -q -f name=devops-container1" ]; then
                             docker rm -f devops-container1
             fi
@@ -15,7 +14,7 @@ node {
             if [  "$(docker ps -q -f name=balancer)" ]; then
                             docker rm -f balancer
             fi
-            echo Correr nuevo contenedor...
+            
             docker-compose up -d --build
         '''
     }
