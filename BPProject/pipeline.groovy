0435896eb211fc757804649f5e8981b43f09f43d
eduar2 <eduar2@localhost.localdomain>
2019-06-16 18:20:25 -0500

    adding stage in jenkinsfile

--- BPProject/pipeline.groovy
+++ BPProject/pipeline.groovy
@@ -1,6 +1,27 @@
 node {
-  stage('GitCheckout') {
-    checkout scm
-  }
+    stage('GitCheckout') {
+        checkout scm
+    }
+
+    stage('Build') {
+        sh '''
+            echo Delete old containers
+            if [  "$(docker ps -q -f name=devops-container1" ]; then
+                            docker rm -f devops-container1
+            fi
+            if [  "$(docker ps -q -f name=devops-container2)" ]; then
+                            docker rm -f devops-container2
+            fi
+            if [  "$(docker ps -q -f name=balancer)" ]; then
+                            docker rm -f balancer
+            fi
+            echo Correr nuevo contenedor...
+            docker-compose up -d --build
+        '''
+    }
+  
+    stage('End ') {
+        ssh " echo 'Succesfull execution)'"
+    }
 
 } 
