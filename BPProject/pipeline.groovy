5d32292bc93c4ca0ce4637a9642d5b27d69f1636
eduar2 <eduar2@localhost.localdomain>
2019-06-16 19:34:03 -0500

    Correct jenkins

--- BPProject/pipeline.groovy
+++ BPProject/pipeline.groovy
@@ -1,26 +1,30 @@
-node {
-    stage('GitCheckout') {
-        checkout scm
-    }
-
-    stage('Build') {
-        sh '''
-            if [  '$(docker ps -q -f name=devops-container1' ]; then
-                            docker rm -f devops-container1
-            fi
-            if [  '$(docker ps -q -f name=devops-container2)' ]; then
-                            docker rm -f devops-container2
-            fi
-            if [  '$(docker ps -q -f name=balancer)' ]; then
-                            docker rm -f balancer
-            fi
+pipeline {
+	agent { label 'docker' }
+	stages {
+        stage('GitCheckout') {
+            steps{
+                checkout scm
+            }
             
-            docker-compose up -d --build
-        '''
-    }
-  
-    stage('End ') {
-        echo "Succesfull execution"
-    }
+        }
 
-} 
+		stage ('build') {
+			steps{
+                sh '''
+                    if [  '$(docker ps -q -f name=devops-container1' ]; then
+                                    docker rm -f devops-container1
+                    fi
+                    if [  '$(docker ps -q -f name=devops-container2)' ]; then
+                                    docker rm -f devops-container2
+                    fi
+                    if [  '$(docker ps -q -f name=balancer)' ]; then
+                                    docker rm -f balancer
+                    fi
+                    
+                    docker-compose up -d --build
+                '''
+            }
+		}
+		
+	}
+}
